d9dc4ecec4001718526dfcd5ea46e56e4d80d330
RLA6 <99086080@mydavinci.nl>
2026-02-06 19:19:32 +0100

    Aantekeningen verwijdert, opdracht Af

--- Quiz.js
+++ Quiz.js
@@ -1,21 +1,3 @@
-/*  
-  DIEREN WEETJES QUIZ
-
-  WAT MOET DE QUIZ KUNNEN
-  - Vragen tonen op het scherm
-  - Antwoorden laten kiezen via knoppen
-  - Controleren of een antwoord juist of fout is
-  - Meerdere vragen achter elkaar afhandelen
-  - Een eindresultaat tonen
-
-  BEOORDELINGSCRITERIA 
-  - Werkende quiz met minimaal 5 vragen
-  - Correct gebruik van JavaScript-concepten
-  - Codekwaliteit (leesbaarheid, structuur, functies)
-  - GitHub structuur
-  - Duidelijke en logische commits
-*/
-
 const vragen = [
     {
         vraag: 'Welk dier kan zichzelf letterlijk weer jong maken en is dus onsterfelijk?',
