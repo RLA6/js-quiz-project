a5411467ffcc15d5eaa3b6e5381c6bbabb7f3801
RLA6 <99086080@mydavinci.nl>
2026-02-06 15:33:24 +0100

    Aantekeningen + benodigde files

---
+++ Quiz.js
@@ -0,0 +1,16 @@
+/* 
+ WAT MOET DE QUIZ KUNNEN
+ Vragen tonen op het scherm
+ Antwoorden laten kiezen via knoppen
+ Controleren of een antwoord juist of fout is
+ Meerdere vragen achter elkaar afhandelen
+ Een eindresultaat tonen
+
+ BEOORDELINGSCRITERIA 
+ Werkende quiz met minimaal 5 vragen
+ Correct gebruik van JavaScript-concepten
+ Codekwaliteit (leesbaarheid, structuur, functies)
+ GitHub structuur
+ Duidelijke en logische commits
+*/
+
